ce4946954dec1fbd3f4ed90413006c8f4d57d04a
CraigJensen <cmjensen82@gmail.com>
2021-12-06 21:44:47 -0600

    no new progress, done for the night

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 var newDay = moment().format('MMMM Do YYYY, h:mm a');
-console.log(newDay);
 
 var currentDay = document.querySelector('#currentDay');
 currentDay.innerHTML = newDay;
 
-var scheduleContainer = document.querySelector('.container');
-var scheduleTime = document.createElement('div');
-var scheduleTask = document.createElement('div');
-var saveTask = document.createElement('div');
-var saveTaskIcon = document.createElement('i');
+for (var i = 0; i < 9; i++) {
+  var scheduleContainer = document.querySelector('.container');
+  var scheduleTime = document.createElement('div');
+  var scheduleTask = document.createElement('div');
+  var saveTask = document.createElement('div');
+  var saveTaskIcon = document.createElement('i');
 
-scheduleTime.className = 'col-lg-1 hour';
-scheduleTask.className = 'col-lg-10 present row';
-saveTask.className = 'col-lg-1 saveBtn';
-saveTaskIcon.className = 'fas fa-save';
+  scheduleTime.className = 'col-lg-1 hour';
+  scheduleTask.className = 'col-lg-10 future row';
+  saveTask.className = 'col-lg-1 saveBtn';
+  saveTaskIcon.className = 'fas fa-save';
 
-scheduleContainer.append(scheduleTime, scheduleTask, saveTask);
+  scheduleContainer.append(scheduleTime, scheduleTask, saveTask);
 
-saveTask.appendChild(saveTaskIcon);
+  saveTask.appendChild(saveTaskIcon);
+}
 
-$('.container').on('click', 'i', function () {
+$('.container').on('click', '.saveBtn', function () {
   console.log('was clicked');
 });
