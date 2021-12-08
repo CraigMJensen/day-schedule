48d3d43aa746bf59d69ae9e903e101fc5ecac170
craigmjensen <cmjensen82@gmail.com>
2021-12-08 15:42:21 -0600

    Added array of hours

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
+var hours = [
+  {
+    hour: 9,
+    meridiem: 'AM',
+  },
+  {
+    hour: 10,
+    meridiem: 'AM',
+  },
+  {
+    hour: 11,
+    meridiem: 'AM',
+  },
+  {
+    hour: 12,
+    meridiem: 'PM',
+  },
+  {
+    hour: 1,
+    meridiem: 'PM',
+  },
+  {
+    hour: 2,
+    meridiem: 'PM',
+  },
+  {
+    hour: 3,
+    meridiem: 'PM',
+  },
+  {
+    hour: 4,
+    meridiem: 'PM',
+  },
+  {
+    hour: 5,
+    meridiem: 'PM',
+  },
+];
+
 var newDay = moment().format('MMMM Do YYYY, h:mm a');
 
 var currentDay = document.querySelector('#currentDay');
-currentDay.innerHTML = newDay;
+currentDay.textContent = newDay;
 
-for (var i = 0; i < 9; i++) {
+for (var i = 0; i < hours.length; i++) {
   var scheduleContainer = document.querySelector('.container');
   var scheduleTime = document.createElement('div');
+  var timeHour = document.createElement('p');
   var scheduleTask = document.createElement('div');
   var saveTask = document.createElement('div');
   var saveTaskIcon = document.createElement('i');
 
   scheduleTime.className = 'col-lg-1 hour';
-  scheduleTask.className = 'col-lg-10 future row';
+  scheduleTask.className = 'col-lg-10 past row text';
   saveTask.className = 'col-lg-1 saveBtn';
   saveTaskIcon.className = 'fas fa-save';
 
-  scheduleContainer.append(scheduleTime, scheduleTask, saveTask);
+  timeHour.textContent = hours[i].hour + hours[i].meridiem;
 
+  scheduleContainer.append(scheduleTime, scheduleTask, saveTask);
+  scheduleTime.appendChild(timeHour);
   saveTask.appendChild(saveTaskIcon);
 }
 
-$('.container').on('click', '.saveBtn', function () {
+$('.container').on('click', '.text', function () {
   console.log('was clicked');
 });
+
+setTimeout(function () {
+  location = '';
+}, 60000);
